9a8948976b55e218840c3fe734e5f6e29c33fa92
Ghaeun Lee <sallyoo1212@gmail.com>
2025-08-21 15:44:55 +0900

    0821 15: 44 땡기기 전 커밋 (api 파일: 스프링부트 연결용인데 백엔드 기능 추가된 게 없어서 잠시 주석 처리)

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,6 @@
-import axios from "axios";
-
-export const api = axios.create({
-    baseURL: "http://localhost:8090", // 백엔드 주소
-    withCredentials: false,           // 세션/쿠키 쓰면 true
-});
+// import axios from "axios";
+//
+// export const api = axios.create({
+//     baseURL: "http://localhost:8090", // 백엔드 주소
+//     withCredentials: false,           // 세션/쿠키 쓰면 true
+// });
